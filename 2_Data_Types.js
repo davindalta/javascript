80a6415888c58dd1bca56345b0de1915d922e2aa
Davin <109206537+davindalta@users.noreply.github.com>
2022-11-11 20:46:09 +0700

    Add files via upload

---
+++ 2_Data_Types.js
@@ -0,0 +1,48 @@
+// seperti bahasa pemrograman lainnya
+// javascript memiliki tipe data :
+// String, number, boolean, null dan undefiend
+
+// tipe data string dapat berupa huruf atau angka asal dalam tanda petik "" atau ''
+// contoh :
+let nama = "Davin";
+let nomor = "11";
+console.log(nama);
+console.log(typeof nama); // cara mengecek tipe data
+
+console.log(nomor);
+console.log(typeof nomor); // cek tipe data menggunakan typeof
+
+
+
+// tipe data number berisi angka
+// contoh:
+let angka = 11;
+console.log(angka);
+console.log(typeof angka);
+
+
+
+// tipe data boolean ada dua yaitu true dan false
+let typeBooleanTrue = true;
+let typeBooleanFalse = false;
+
+console.log(typeBooleanTrue);
+console.log(typeof typeBooleanTrue)
+
+console.log(typeBooleanFalse);
+console.log(typeof typeBooleanFalse); 
+
+
+
+// tipe data null adalah tipe data yang tidak memiliki nilai
+// contoh:
+let dataTipeNull = null;
+console.log(dataTipeNull);
+console.log(typeof dataTipeNull);
+
+
+//undefined adalah tipe data yang tidak ada nilainya, atau belum di deklarasikan
+// contoh:
+let tipeDataUndefined;
+console.log(tipeDataUndefined);
+console.log(typeof tipeDataUndefined);
